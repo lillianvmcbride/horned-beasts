896dec8ae79a6a4a8272a960291a61d01fc6763d
Lilly McBride <lillianvioletmcbride@gmail.com>
2022-05-10 02:02:39 -0500

    header

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+/**
+ * @file Header.js
+ * @author Lillian McBride
+ * @description Renders the Header for Horned Beasts
+ */
+ import React from "react";
+
+ class Header extends React.Component {
+
+   render() {
+     return <h1 className="h1">${`Horned Beasts`}</h1>;
+   }
+ }
+ 
+ export default Header;
